63dcc180613e5209cb39a41fb469d11d17ddd768
prcordova <pedroricardocordova@yahoo.com.br>
2024-12-03 12:15:25 -0300

    fix: ajusta edição ed endereço usuario

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const path = require("path");
 
 exports.getUsers = async (req, res) => {
   try {
@@ -29,7 +31,7 @@ exports.getUserById = async (req, res) => {
       faceVerified: user.faceVerified,
       role: user.role,
       status: user.status,
-      avatar: user.avatar,
+      avatar: user.avatar ? `/uploads/${user.avatar}` : null, // Corrige o caminho do avatar
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -40,13 +42,13 @@ exports.updateProfile = async (req, res) => {
   try {
     const userId = req.params.id;
     const updates = req.body;
+    console.log("entrou no updateProfile", userId, updates);
 
     // Se houver um arquivo de avatar, atualize o caminho do avatar
     if (req.file) {
-      updates.avatar = path.relative(
-        path.join(__dirname, "../../public"),
-        req.file.path
-      );
+      updates.avatar = path
+        .relative(path.join(__dirname, "../../public/uploads"), req.file.path)
+        .replace(/\\/g, "/"); // Corrige o caminho para usar barras normais
     }
 
     // Atualize o endereço se fornecido
@@ -55,10 +57,39 @@ exports.updateProfile = async (req, res) => {
     }
 
     const user = await User.findByIdAndUpdate(userId, updates, { new: true });
+
     if (!user) {
       return res.status(404).json({ message: "Usuário não encontrado" });
     }
-    res.json(user);
+
+    // Gere um novo token JWT
+    const token = jwt.sign(
+      { sub: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    // Armazena o novo token em um cookie
+    res.cookie("session-token", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+      access_token: token,
+      role: user.role,
+      userId: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      cpf: user.cpf,
+      phone: user.phone,
+      birthDate: user.birthDate,
+      address: user.address,
+      hasAcceptedTerms: user.hasAcceptedTerms,
+      termsAcceptedDate: user.termsAcceptedDate,
+      workerDetails: user.workerDetails,
+      avatar: user.avatar,
+    });
   } catch (err) {
     console.error("Error updating profile:", err);
     res.status(500).json({ message: "Error updating profile." });
