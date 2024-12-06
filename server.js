ad6001c2dcec79005851bdf51c5e8d9384c647e8
prcordova <pedroricardocordova@yahoo.com.br>
2024-12-06 13:57:08 -0300

    fix: changes ports

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const app = require("./src/app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 4000;
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -11,9 +9,7 @@ mongoose
   })
   .then(() => {
     console.log("Conectado ao MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
+    app.listen(8080 || 4000);
   })
   .catch((err) => {
     console.error("Erro ao conectar ao MongoDB:", err);
